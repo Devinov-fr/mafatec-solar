f059aa1e8629dc54b35d9ae1ac4545ef9ad4828e
Ryhab Ghammem <ryhab@devinov.fr>
2026-02-24 15:14:08 +0100

    test

--- app/(etude)/page.tsx
+++ app/(etude)/page.tsx
@@ -6,7 +6,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
 import dynamic from "next/dynamic";
 
-import Footer from "@/components/ui/Footer";
+import Footer from "@/components/ui/footer";
 
 import Header from "@/components/ui/Header";
 import Hero from "@/components/ui/Hero";
